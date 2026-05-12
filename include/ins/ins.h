846af6054c95d96bfbe4b7b2fab438b7b301824a
fish <i@coraa.cn>
2026-05-12 20:15:12 +0800

    feat: 支持初始化时传入旋转矩阵

--- include/ins/ins.h
+++ include/ins/ins.h
@@ -6,16 +6,16 @@
 
 #include <cstdint>
 #include <bsp/imu.h>
+#include <math/matrix.h>
 
 namespace ins {
     struct data_t {
-        float q[4];
+        float q[4], accel[3], gyro[3];
         float roll, pitch, yaw;
         float yaw_total_angle;
-        uint32_t timestamp;
         bsp_imu_data_t raw;
     };
     inline bool inited = false;
-    void init();
+    void init(const math::matrix<3, 3> &trans = math::matrix<3, 3>::eye());
     data_t *data();
 }
--- src/ins.cc
+++ src/ins.cc
@@ -17,6 +17,7 @@
 using namespace ins;
 
 namespace ins {
+    math::matrix<3, 3> _trans;
     data_t _data;
     static struct {
         float gyro_corr[3] = { 0, 0, 0 };
@@ -29,6 +30,11 @@ static TaskHandle_t task_handle;
 [[noreturn]] static void task(void *args) {
     IMU_QuaternionEKF_Init(10, 0.001, 10000000, 1, 0);
     logger::info("module inited");
+    logger::info("transform matrix: [%f, %f, %f; %f, %f, %f; %f, %f, %f]",
+        _trans(0, 0), _trans(0, 1), _trans(0, 2),
+        _trans(1, 0), _trans(1, 1), _trans(1, 2),
+        _trans(2, 0), _trans(2, 1), _trans(2, 2)
+    );
 
     osDelay(50);
 
@@ -55,11 +61,20 @@ static TaskHandle_t task_handle;
     logger::info("done");
 
     uint32_t lst_wakeup_time = bsp_time_get_ms();
+    math::matrix<3, 1> t_gyro, t_accel;
 
     for (;;) {
         _data.raw = bsp_imu_read();
-        _data.raw.gyro[0] -= _cali_data.gyro_corr[0], _data.raw.gyro[1] -= _cali_data.gyro_corr[1], _data.raw.gyro[2] -= _cali_data.gyro_corr[2];
-        IMU_QuaternionEKF_Update(_data.raw.gyro[0], _data.raw.gyro[1], _data.raw.gyro[2], _data.raw.accel[0], _data.raw.accel[1], _data.raw.accel[2], 0.001);
+        memcpy(_data.gyro, _data.raw.gyro, sizeof _data.gyro);
+        memcpy(_data.accel, _data.raw.accel, sizeof _data.accel);
+        _data.gyro[0] -= _cali_data.gyro_corr[0], _data.gyro[1] -= _cali_data.gyro_corr[1], _data.gyro[2] -= _cali_data.gyro_corr[2];
+
+        t_gyro.load(_data.gyro);
+        t_accel.load(_data.accel);
+        (_trans * t_gyro).save(_data.gyro);
+        (_trans * t_accel).save(_data.accel);
+
+        IMU_QuaternionEKF_Update(_data.gyro[0], _data.gyro[1], _data.gyro[2], _data.accel[0], _data.accel[1], _data.accel[2], 0.001);
         _data.roll = QEKF_INS.Roll, _data.pitch = QEKF_INS.Pitch, _data.yaw = QEKF_INS.Yaw, _data.yaw_total_angle = QEKF_INS.YawTotalAngle;
         _data.roll *= M_PI / 180.f;
         _data.pitch *= M_PI / 180.f;
@@ -70,8 +85,9 @@ static TaskHandle_t task_handle;
     }
 }
 
-void ins::init() {
-    xTaskCreate(task, "ins", 128, nullptr, osPriorityRealtime, &task_handle);
+void ins::init(const math::matrix<3, 3> &trans) {
+    _trans = trans;
+    xTaskCreate(task, "ins", 256, nullptr, osPriorityRealtime, &task_handle);
 }
 
 data_t *ins::data() {
